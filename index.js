dd91ef20950da5ddef4cb0ed442122de35244b1e
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-08-05 09:40:21 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 var cors = require("cors");
 const bodyParser = require("body-parser");
 
